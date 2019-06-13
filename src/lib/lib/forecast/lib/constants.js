1a806f4e8b65ef598d506a3889967686c49f3dfc
Husseiny Mohamed <husseiny@venzee.com>
2019-06-13 10:44:21 +0200

    added skeleton forecast module and installed axios

---
+++ src/lib/lib/forecast/lib/constants.js
@@ -0,0 +1,6 @@
+const api = {
+  baseUrl: 'https://api.openweathermap.org/data/2.5/weather',
+  appId: 'e1d4d70a863323bc102d5527d06faf08' // this should not be clear text here, but for lake of time kept it this way
+};
+
+module.exports = { api };
