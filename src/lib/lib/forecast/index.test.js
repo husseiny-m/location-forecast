efea71b5a6499d9a57692637ed7253bea4e7152a
Husseiny Mohamed <husseiny@venzee.com>
2019-06-13 11:07:30 +0200

    added getForecastByPostalCode unit tests

--- src/lib/lib/forecast/index.test.js
+++ src/lib/lib/forecast/index.test.js
@@ -1,5 +1,28 @@
+const {getForecastByPostalCode} = require('./index');
+
+const axios = require('axios');
+jest.mock('axios');
+
+const resp = {data: {main: {temp: 297.92, pressure: 1008, humidity: 57, temp_min: 285.15, temp_max: 302.04}}};
+
 describe('index.js', () => {
-  it('sample test', () => {
-    expect(1).toEqual(1);
+  describe('getForecastByPostalCode', () => {
+    beforeEach(() => {
+      axios.get.mockImplementation(() => Promise.resolve(resp));
+    });
+
+    it('should have getForecastByPostalCode function', () => {
+      expect(getForecastByPostalCode).toBeDefined();
+    });
+
+    it('should call axios.get once from forecast module', async () => {
+      await getForecastByPostalCode();
+      expect(axios.get).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return valid data ', async () => {
+      const actualData = await getForecastByPostalCode();
+      expect(actualData).toEqual(resp.data);
+    });
   });
 });
